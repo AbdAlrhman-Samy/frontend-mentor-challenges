349f12d3329234f1f8b8c05b0c44203aa4d4b2f2
AbdAlrhman Samy <abdalrhman.samy@outlook.com>
2023-01-29 19:10:35 +0200

    finished most of the ui and some of the functionality

---
+++ todo-app/JS/app.js
@@ -0,0 +1,41 @@
+const checkBtns = document.querySelectorAll(".todo__check-button")
+console.log(checkBtns);
+
+Array.from(checkBtns).forEach(btn => {
+    btn.addEventListener("click", (e)=>{
+        e.target.classList.toggle("todo__check-button--checked")
+        e.target.parentElement.classList.toggle("todo__item--checked")
+    })
+})
+
+// Theme Handler
+let theme;
+
+const checkTheme = () => {
+    const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    let savedTheme = localStorage.getItem("theme")
+    if(savedTheme){
+        document.documentElement.setAttribute("data-theme", savedTheme)
+    } else {
+        if(prefersDarkTheme) {
+            localStorage.setItem("theme", "dark")
+            theme = "dark"
+        }
+
+        else{
+            localStorage.setItem("theme", "light")
+            theme = "light"
+        }  
+        
+        document.documentElement.setAttribute("data-theme", theme)
+
+    }
+}
+
+const toggleTheme = () => {
+    theme = theme==="dark" ? "light" : "dark"
+    document.documentElement.setAttribute("data-theme", theme)
+    localStorage.setItem("theme", document.documentElement.getAttribute("data-theme"))
+}
+
+checkTheme()
