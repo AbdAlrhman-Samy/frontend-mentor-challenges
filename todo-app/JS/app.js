ee80353066b7b174b7c6ed50d669f2b854396b1d
AbdAlrhman Samy <abdalrhman.samy@outlook.com>
2023-01-30 00:40:00 +0200

    finished most of the functionality, code refactoring is needed

--- todo-app/JS/app.js
+++ todo-app/JS/app.js
@@ -1,16 +1,96 @@
-window.addEventListener("load", ()=>{
-    const body = document.documentElement.style.height = window.innerHeight
+const todoList = document.querySelector(".todo__list")
+const todoInput = document.querySelector(".data__input")
+const remainingTodos = document.querySelector(".todo__count")
+let todoCount;
+let TODOS = [];
+
+window.addEventListener("load", () => {
+    document.documentElement.style.height = window.innerHeight
 })
 
-const checkBtns = document.querySelectorAll(".todo__check-button")
-console.log(checkBtns);
+const updateRemaining = () => {
+    remainingTodosCount = TODOS.filter(todo => todo.isDone === false).length
+    remainingTodos.innerHTML = `<b>${remainingTodosCount}</b> items remaining`
+}
 
-Array.from(checkBtns).forEach(btn => {
-    btn.addEventListener("click", (e)=>{
-        e.target.classList.toggle("todo__check-button--checked")
-        e.target.parentElement.classList.toggle("todo__item--checked")
+function Todo(text){
+    this.text = text
+    this.isDone = false
+    this.id = Math.round(Math.random() * 10000)
+}
+
+const createTodo = (todo) => {
+    const todoTemplate = `
+    <li class="todo__item ${todo.isDone? "todo__item--checked" : ""}" data-id=${todo.id}>
+        <button class="todo__check-button ${todo.isDone? "todo__check-button--checked" : ""}" onclick="checkTodo(event)"></button>
+        <span class="todo__text">${todo.text}</span>
+        <button class="todo__remove-button" onclick="removeTodo(event)">
+        </button>
+    </li>
+    `;
+
+    todoList.insertAdjacentHTML("beforeend", todoTemplate)
+
+}
+
+const getTodos = () => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"))
+    if(savedTodos){
+        TODOS = savedTodos
+        savedTodos.forEach(todo => createTodo(todo))
+        updateRemaining()
+    } else console.log('none :(');
+}
+getTodos()
+
+todoInput.addEventListener("keyup", (e)=>{
+    if(e.key === "Enter"){
+        const newTodo = new Todo(e.target.value);    
+        createTodo(newTodo)
+        TODOS = [...TODOS, newTodo]
+        localStorage.setItem("todos", JSON.stringify(TODOS))
+    }
+})
+
+
+const checkTodo = (e) => {
+    const id = e.target.parentElement.getAttribute("data-id")
+    e.target.classList.toggle("todo__check-button--checked")
+    e.target.parentElement.classList.toggle("todo__item--checked")
+    TODOS = TODOS.map(todo => {
+        if(todo.id === Number(id)){
+            return {...todo, isDone: !todo.isDone}
+        } return todo
     })
-})
+    localStorage.setItem("todos", JSON.stringify(TODOS))
+    updateRemaining()
+}
+
+
+const removeTodo = (e) => {
+    const id = e.target.parentElement.getAttribute("data-id")
+    TODOS = TODOS.filter(todo => todo.id !== Number(id))
+    console.log(id);
+    const element = e.target.parentElement
+    element.remove()
+    localStorage.setItem("todos", JSON.stringify(TODOS))
+    updateRemaining()
+}
+
+const clearCompleted = () => {
+    console.log('clear');
+    TODOS = TODOS.filter(todo => !todo.isDone)
+    localStorage.setItem("todos", JSON.stringify(TODOS))
+
+}
+
+
+
+// const filterTodos = (filter) => {
+//     if(filter === "all"){
+
+//     }
+// }
 
 // Theme Handler
 let theme;
